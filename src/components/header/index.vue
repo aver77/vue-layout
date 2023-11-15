07e34e5eb04e4408bc46053f936f392ccf31971a
Nikita Averochkin <dota.nikita@inbox.ru>
2023-11-15 16:19:59 +0300

    hotfix

--- src/components/header/index.vue
+++ src/components/header/index.vue
@@ -1,15 +1,15 @@
 <script lang="ts" setup>
 import Links from "../links/index.vue";
 import ButtonComponent from "@/shared/ui/components/button/index.vue";
+
+import pdfCV from "@/shared/assets/pdf/NikitaAverochkinCV.pdf";
 </script>
 
 <template>
     <header :class="$style.container">
         <Links />
         <ButtonComponent :class="$style.btn"
-            ><a href="src/shared/assets/pdf/NikitaAverochkinCV.pdf"
-                >My Resume</a
-            ></ButtonComponent
+            ><a :href="pdfCV" target="_blank">My Resume</a></ButtonComponent
         >
     </header>
 </template>
