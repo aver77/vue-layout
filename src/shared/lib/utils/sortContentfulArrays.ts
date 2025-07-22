9170b950859bb8025cb6de9ff944bdd4263dc098
Nikita Averochkin <n.averochkin@corp.mail.ru>
2025-07-22 14:35:31 +0300

    edited sort arrays func

--- src/shared/lib/utils/sortContentfulArrays.ts
+++ src/shared/lib/utils/sortContentfulArrays.ts
@@ -1,7 +1,13 @@
 import type { IContentfulResourceFields } from '@/shared/ts/contentful';
 
 export const sortContentfulArrays = <T extends { id: number }>(
-    array: IContentfulResourceFields<T>[]
+    array: IContentfulResourceFields<T>[],
+    asc?: boolean
 ) => {
-    return array?.sort((item1, item2) => item1.fields.id - item2.fields.id);
+    return array?.sort((item1, item2) => {
+        const id1 = asc ? item1.fields.id : item2.fields.id;
+        const id2 = asc ? item2.fields.id : item1.fields.id;
+
+        return id1 - id2;
+    });
 };
